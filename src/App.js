f64aac36d5ba088f8ec4257b32c46d8adc31053e
minseok128 <minseok128128@gmail.com>
2022-08-07 23:04:04 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
-    <Routes>
-      <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
-      <Route path="/movie/:id" element={<Detail />} />
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
