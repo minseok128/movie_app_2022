48e836332e8faeb67714394af4f7bc51c29f17e6
minseok128 <minseok128128@gmail.com>
2022-08-07 22:44:20 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
       <Route path="/movie/:id" element={<Detail />} />
     </Routes>
   );
